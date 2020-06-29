23dcaaaede5755c5d7c6002c58d040747516d0b2
Cataline Baert <snuf_kin@yahoo.com>
2020-06-29 20:55:54 +0200

    display search bar and all results; add some style

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import algoliasearch from 'algoliasearch';
+import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
 import React from 'react';
 import './App.css';
 
-const client = algoliasearch('LRNJXNO42B', '25b590194234b396adb5f47435dd483d');
+const searchClient = algoliasearch('LRNJXNO42B', '25b590194234b396adb5f47435dd483d');
 
-const index = client.initIndex('actors');
+const index = searchClient.initIndex('talks');
 
-fetch('https://raw.githubusercontent.com/algolia/datasets/master/movies/actors.json')
+fetch(
+  "https://raw.githubusercontent.com/algolia/datasets/master/tedtalks/talks.json"
+)
   .then(function (response) {
-    return response.json()
+    return response.json();
   })
-  .then(function (actors) {
-    return index.saveObjects(actors, {
-      autoGenerateObjectIDIfNotExist: true
-    })
-  })
-
-index.search('williams').then(({ hits }) => {
-  console.log(hits);
-});
+  .then(function (talks) {
+    return index.saveObjects(talks, {
+      autoGenerateObjectIDIfNotExist: true,
+    });
+  });
 
 function App() {
+
+  const Hit = ({ hit }) => {
+    return (
+      <div className="Hit">
+        <h3 className="Hit-title">{hit.name}</h3>
+    <p className="Hit-speaker">{hit.speakers[0]}</p>
+        <img className="Hit-image" src={hit.image_url} alt=""/>
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <h1>Algolia Search</h1>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName="talks">
+      <h1 id="title">Ted Talks Search Engine</h1>
+      <div id="searchbox">
+        {<SearchBox translations={{ placeholder: "Search for Ted talks" }} />}
+      </div>
+      <div id="hits">
+        {<Hits hitComponent={Hit}/>}
+      </div>
+    </InstantSearch>
   );
 }
 
